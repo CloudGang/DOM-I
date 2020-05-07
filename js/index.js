c77e8bf7ea5d1a5bca980d5125a9e0b5522402e8
cloudgang <35246400+CloudGang@users.noreply.github.com>
2020-05-06 22:31:42 -0400

    Complete w/ Stretches

--- js/index.js
+++ js/index.js
@@ -40,3 +40,66 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let ctaImg = document.getElementById("cta-img");
+ctaImg.setAttribute('src', siteContent["cta"]["img-src"])
+
+let middleImg = document.getElementById("middle-img");
+middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+///////////////////////////////////////////////////////////////////////////
+//////////////////////// TOP CONTENT //////////////////////////////////////
+let ctaText = document.getElementsByClassName('cta-text')[0]
+console.log(ctaText)
+ctaText.getElementsByTagName('h1')[0].innerHTML = siteContent.cta.h1
+let btn = document.querySelector(".cta button");
+btn.textContent = siteContent.cta.button;
+///////////////////////////////////////////////////////////////////////////
+///////////////////////// N A V ///////////////////////////////////////////
+let navLinks = document.querySelectorAll('nav a')
+console.log(navLinks)
+navLinks.forEach(function(link, i){
+  link.textContent = siteContent.nav[`nav-item-${i + 1}`]
+})
+///////////////////////////////////////////////////////////////////////////
+////////////////////////// B O D Y ////////////////////////////////////////
+let bodyH = document.querySelectorAll(".text-content h4");
+let bodyP = document.querySelectorAll(".text-content p");
+bodyH[0].textContent = siteContent["main-content"]["features-h4"];
+bodyH[1].textContent = siteContent["main-content"]["about-h4"];
+bodyH[2].textContent = siteContent["main-content"]["services-h4"];
+bodyH[3].textContent = siteContent["main-content"]["product-h4"];
+bodyH[4].textContent = siteContent["main-content"]["vision-h4"];
+bodyP[0].textContent = siteContent["main-content"]["features-content"];
+bodyP[1].textContent = siteContent["main-content"]["about-content"];
+bodyP[2].textContent = siteContent["main-content"]["services-content"];
+bodyP[3].textContent = siteContent["main-content"]["product-content"];
+bodyP[4].textContent = siteContent["main-content"]["vision-content"];
+///////////////////////////////////////////////////////////////////////////
+////////////////////// CONTACT ////////////////////////////////////////////
+contHead = document.querySelector(".contact h4");
+contText = document.querySelectorAll(".contact p");
+contHead.textContent = siteContent.contact["contact-h4"];
+contText[0].textContent = siteContent.contact.address;
+contText[1].textContent = siteContent.contact.phone;
+contText[2].textContent = siteContent.contact.email;
+contHead.style.color = 'green';
+///////////////////////////////////////////////////////////////////////////
+///////////////////////F O O T E R/////////////////////////////////////////
+document.querySelector("footer p").textContent = siteContent.footer.copyright;
+let footer = document.querySelector('footer p');
+footer.addEventListener('click', (e) => e.target.style.background = 'red');
+///////////////////////////////////////////////////////////////////////////
+///////////////////////S T R E T C H///////////////////////////////////////
+let newTag = document.createElement('a');
+newTag.textContent = "Start";
+let newTag2 = document.createElement('a');
+newTag2.textContent = "Finish";
+const newNavContent = document.querySelector('nav');
+newNavContent.appendChild(newTag);
+newNavContent.prepend(newTag2);
+
+let navChange = document.querySelectorAll('a');
+navChange.forEach(function(currentValue){
+  currentValue.style.color = 'green';
+});
+// E N D //
